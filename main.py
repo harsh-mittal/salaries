17b39ba4b05da8329258453292223a8135807815
harsh <harsh.mail89@gmail.com>
2024-09-21 03:06:25 +0530

    add llm to answer questions based on csv file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # from langchain_community.document_loaders import CSVLoader
+import os
 import pandas as pd
 from dotenv import load_dotenv
 import streamlit as st
+from langchain_groq import ChatGroq
+from langchain_experimental.agents import create_pandas_dataframe_agent
 
 @st.cache_data
 def load_data(file_path = 'salaries.csv'):
@@ -9,6 +12,11 @@ def load_data(file_path = 'salaries.csv'):
         return pd.read_csv(file_path)
     except:
         return None
+
+def handle_error(error):
+    print("Got Error:")
+    print(error)
+    return "Missing context, unable to understand question."
 
 @st.cache_data
 def aggregate_yearly_data(df):
@@ -18,11 +26,16 @@ def aggregate_yearly_data(df):
 def aggregate_job_data(df):
     return df.astype({'work_year': str}).groupby(['work_year', 'job_title']).agg(job_count=('work_year', 'size')).reset_index()
 
+st.session_state["llm_model"] = "llama3-8b-8192"
+
 load_dotenv()
 
 raw_df = load_data()
 yearly_df = aggregate_yearly_data(raw_df)
 job_df = aggregate_job_data(raw_df)
+
+llm = ChatGroq(model=st.session_state.llm_model, api_key=os.environ.get("GROQ_API_KEY"))
+agent_executor = create_pandas_dataframe_agent(llm, raw_df, verbose=False, agent_executor_kwargs={"handle_parsing_errors": handle_error}, allow_dangerous_code=True)
 
 st.title('Salary Analysis')
 
@@ -43,21 +56,16 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("Enter your question here..."):
     assistant_messages_container = st.chat_message("assistant")
     user_messages_container = st.chat_message("user")
 
     user_messages_container.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Echo: {prompt}"
-    assistant_messages_container.markdown(response)
-    st.session_state.messages.append({"role": "assistant", "content": response})
-
-# loader = CSVLoader(file_path="salaries.csv")
-# documents = loader.load()
-
-# embeddings = OpenAIEmbeddings()
-
-# print(type(documents[0]))
-# print(documents[0])
+    try:
+        response = agent_executor.invoke(prompt)
+        assistant_messages_container.markdown(response["output"])
+        st.session_state.messages.append({"role": "assistant", "content": response["output"]})
+    except Exception as e:
+        st.error(e)
